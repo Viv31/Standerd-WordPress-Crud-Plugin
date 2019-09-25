79c4a5c876237a4214c96726c0113c6b6b584705
Vaibhav Gangrade <45598521+Viv31@users.noreply.github.com>
2019-09-25 22:38:47 +0530

    Update alluser.php

--- alluser.php
+++ alluser.php
@@ -19,34 +19,73 @@ function bg_img(){ ?>
     body{
         background-image: url("<?php echo PLUGIN_URL.'/CRUD/img/bg.jpg'?>");
     }
+
+
+
+
+
+
+   div#email_error_msg ,#inserted_success,#update_sucess,#delete_msg {
+    
+
+    -moz-animation: cssAnimation 0s ease-in 5s forwards;
+    /* Firefox */
+    -webkit-animation: cssAnimation 0s ease-in 5s forwards;
+    /* Safari and Chrome */
+    -o-animation: cssAnimation 0s ease-in 5s forwards;
+    /* Opera */
+    animation: cssAnimation 0s ease-in 5s forwards;
+    -webkit-animation-fill-mode: forwards;
+    animation-fill-mode: forwards;
+}
+@keyframes cssAnimation {
+    to {
+        width:0;
+        height:0;
+        overflow:hidden;
+       
+        
+    }
+}
+@-webkit-keyframes cssAnimation {
+    to {
+        width:0;
+        height:0;
+       visibility:hidden;
+      
+        
+
+    }
+}
 </style>
 
 
 <?php
 }
+
+bg_img();
 ?>
 
 <div class="container">
-	<div class="row">
-		<div class="col-md-12">
-            <p id="success_msg"><?php
+
+<?php
             if(isset($_GET['email_exist'])==true){ ?>
                
-                <div class="alert alert-danger fade in">
-                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                <div class="" id="email_error_msg">
+                
   <strong>Error!</strong>Email is already exist
 </div>
                 
            <?php }
             if(isset($_GET['reg_success'])==true){ ?>
-                <div class="alert alert-success fade in">
+                <div class="" id="inserted_success">
                 <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
   <strong>Success!</strong>Data Inserted Successfully
 </div>
             <?php }
 
             if(isset($_GET['update_success'])==true){ ?>
-               <div class="alert alert-success fade in">
+               <div class="" id="update_sucess">
                 <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
   <strong>Success!</strong>Data Updated Successfully
 </div>
@@ -54,13 +93,20 @@ function bg_img(){ ?>
            <?php  }
 
             if(isset($_GET['delete_success'])==true){ ?>
-                <div class="alert alert-success fade in">
-                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                <div class="" id="delete_msg" >
+                
   <strong>Success!</strong>Data Deleted Successfully
 </div>
            <?php }
 
-            ?></p>
+            ?>
+
+
+
+	<div class="row">
+		
+            
+            <div class="col-md-12">
 			<div class="jumbotron">
 				<a href="<?php echo SITE_URL?>/vg-crudpage"><button class="btn btn-primary pull-right">Back</button></a>
 				<h4>Register User List</h4><hr>
